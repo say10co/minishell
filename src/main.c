96624011d11e97ad73369b4934eacabccfaefc0f
Driouch Ayoub <youba@Driouchs-MacBook-Air.local>
2022-03-30 10:07:44 +0000

    No more Open Quote handeling

--- src/main.c
+++ src/main.c
@@ -14,12 +14,13 @@ bool is_even(int x)
 	return (x % 2 == 0);
 }
 
-static bool trace_quote_dquote(char *prefix, char **p_prompt)
+static bool trace_quote_dquote(char *prefix)
 {
 	unsigned int		i;
-	static int			open_quote;
+	unsigned int		open_quote;
 
 	i = 0;
+	open_quote = NONE;
 	while (prefix[i])
 	{
 		if (prefix[i] == '"' && open_quote == NONE)
@@ -34,67 +35,26 @@ static bool trace_quote_dquote(char *prefix, char **p_prompt)
 	}
 	if (open_quote == DQUOTE)
 	{
-		*p_prompt = "dquote> ";
+		ft_putstr_fd("[Error] Double Quote Unclosed!\n" ,2);
 		return (1);
 	}
-	else if (open_quote == QUOTE)
-	{	
-		*p_prompt = "quote> ";
+	if (open_quote == QUOTE)
+	{
+		ft_putstr_fd("[Error] Single Quote Unclosed!\n" ,2);
 		return (1);
 	}
 	return (0);
 }
 
-char *get_valid_line(void)
-{
-	char	*f_command;
-	char	*command;
-	char 	*prompt;
-	char 	*tmp;
-	bool	start;
-	
-	start = 1;
-	prompt = "miniSHELL-0.0$ ";
-	command = readline(prompt);
-	while (1)
-	{
-		if (start)
-		{
-			f_command = ft_strdup(command);
-			start = 0;
-		}
-		else
-		{
-			command = append(command, "\n");
-			tmp = f_command;
-			f_command = ft_strjoin(f_command, command);
-			free(tmp);
-		}
-		if (!trace_quote_dquote(command, &prompt))
-				break;
-		free(command);
-		command = readline(prompt);
-	}
-	if (start)
-		return (command);
-	free(command);
-	return (f_command);	
-}
-
 int main(void)
 {
 	char	*command;
-	char	*t_command;
 
 	while (1)
 	{
-		command = get_valid_line();
-		if (command)
+		command = readline("MiniShell-0.0$ ");
+		if (command && !trace_quote_dquote(command))
 			add_history(command);
-		t_command = ft_strtrim(command, " \t");
-		free(command);
-		command = t_command;
-		printf("%s\n", t_command);
 		//system(command);
 		free(command);
 	}
