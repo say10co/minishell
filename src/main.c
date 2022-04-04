db812366f17a078052897bb780a3f19e4f89c806
Ayoub Driouich <adriouic@e1r13p8.1337.ma>
2022-04-04 18:34:11 +0000

    getting files descriptors of files

--- src/main.c
+++ src/main.c
@@ -6,9 +6,11 @@
 /*   By: adriouic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 11:19:28 by adriouic          #+#    #+#             */
-/*   Updated: 2022/04/03 20:17:14 by adriouic         ###   ########.fr       */
+/*   Updated: 2022/04/04 18:26:31 by adriouic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <fcntl.h>
+
 #include "../includes/includes.h"
 char	**my_ft_split(const char *str, char c);
 enum
@@ -19,13 +21,15 @@ enum
 	DLARROW,
 };
 
-typedef struct s_simple_command
+typedef struct s_command
 {
-	char *simple_command;
+	char 	**cmd;
 	int		in_file;
 	int		out_file;
+	int		nb_pipes;
+	int		append;
 
-}t_simple_command;
+}t_command;
 
 char *polish(const char *s, char *charset);
 
@@ -104,8 +108,6 @@ void remove_quotes(char **all)
 	}
 }
 
-void	expand_variables(char **all);
-
 void	open_deocument(char *eof)
 {
 	char *buffer;
@@ -122,64 +124,110 @@ void	open_deocument(char *eof)
 	}
 }
 
-void parser(char *field)
+char *expand_variables(char *key)
+{
+	return (key);
+}
+
+int	open_file(char *file_name, int flag)
+{
+	int fd;
+
+	if (file_name[0] == '$')
+		expand_variables(file_name + 1);
+	if (flag == O_WRONLY)
+		fd = open(file_name, flag | O_CREAT);
+	else
+		fd = open(file_name, flag);
+	printf("fd %d \n", fd);
+	return (fd);
+}
+
+t_command *parser(char *field)
 {
 	int	i;
 	char **all;
-	char *outfile;
-	char *infile;
+	t_command *command;
 
 	i = 0;
-	infile = NULL;
-	outfile = NULL;
+	command = (t_command *)malloc(sizeof(t_command));
 	all = my_ft_split(field, ' ');
+	command->cmd = NULL; 
 	remove_quotes(all);
-	//expand_variables(all);
 	while (all[i])
 	{
 		if (!ft_strcmp(all[i], ">") || !ft_strcmp(all[i], ">>"))
-			outfile = all[i + 1];
-		if (!ft_strcmp(all[i], "<"))
-			infile = all[i + 1];
-		if (!ft_strcmp(all[i], "<<"))
-			open_deocument(all[i + 1]);
+		{
+			command->append = !ft_strcmp(all[i], ">>");
+			command->out_file = open_file(all[++i], O_WRONLY);
+		}
+		else if (!ft_strcmp(all[i], "<"))
+			command->in_file = open_file(all[++i], O_WRONLY);
+		else if (!ft_strcmp(all[i], "<<"))
+			open_deocument(all[++i]);
 		i++;
 	}
-	printf("IN FILE:[%s]\n", infile);
-	printf("OUT FILE:[%s]\n", outfile);
+	return(command);
 }
 
-bool get_data_from_subfield(char **fields)
+bool get_data_from_subfield(int ac, char **fields, t_command ***all_commands)
 {
 	int i;
 
 	i = 0;
+	*all_commands = (t_command **) malloc(sizeof(t_command) * ac);
 	while (fields[i])
 	{
 		if (veriffy_syntax(fields[i]))
 			return 1;
-		parser(fields[i]);
+		(*all_commands)[i] = parser(fields[i]);
 		i++;
 	}
 	return (0);
 }
 
-int main(void)
+t_list *create_env(char **env)
 {
-	char **argv;
-	int		ac;
+	t_list	*head;
+	t_list	*tmp;
+	int		i;
 
+	head = NULL;
+	i = 0;
+	while (env[i])
+	{
+		tmp = ft_lstnew(env[i]);
+		if (!tmp)
+		{
+			ft_lstclear(&head, free);
+			return (NULL);
+		}	
+		ft_lstadd_front(&head, tmp);
+		i++;
+	}
+	return (head);
+}
+
+int main(int ac, char **argv, char **env)
+{
+	t_list		*enviorment;
+	t_command	**all_commands;
+	
+	enviorment = create_env(env);
 	argv = NULL;
+	all_commands = NULL;
 	while (1)
 	{
 		ac = get_pipe_fileds(&argv);
 		if (!ac)
 			continue ;
-		get_data_from_subfield(argv);
-		continue;
+		get_data_from_subfield(ac, argv, &all_commands);
 		for(int i = 0; (argv && argv[i] != NULL); i++)
 		{
-			printf("Field %d: %s \n", i, argv[i]);
+			printf("_________Field %d ______:\n	::: %s \n", i, argv[i]);
+			printf("command %s\n", NULL);
+			printf("input fd : %d\n", all_commands[i]->in_file);
+			printf("output fd : %d\n", all_commands[i]->out_file);
 			free(argv[i]);
 		}
 		free(argv);
