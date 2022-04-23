d65e50829183d1e4b7fdd4e6b73133e01235ce52
Brahim Berkasse <berkassebrahim@gmail.com>
2022-04-23 21:44:02 +0000

    🧸 refactor cd !

--- src/builtin/b_cd.c
+++ src/builtin/b_cd.c
@@ -18,6 +18,14 @@ static int arg_size(char **arg)
 	return (l);
 }
 
+static void go_tohome()
+{
+    int status;
+
+    status = chdir(ft_getenv("HOME"));
+		if(status == -1)
+			perror("cd faild !");
+}
 static void go_todir(char *dir)
 {
 	int status;
@@ -27,57 +35,72 @@ static void go_todir(char *dir)
 		printf("cd: no such file or directory: %s\n", dir);
 }
 
+static void go_tobasedhome(char *dir)
+{
+    char *holder;
+    char *pwd;
+    int status;
+
+    holder = ft_getenv("HOME");
+		pwd = ft_strreplace(dir, "~", holder);
+		status = chdir(pwd);
+		free(pwd);
+		if(status == -1)
+			printf("cd: no such file or directory: %s\n", dir);
+}
+
+static void go_replcpwd(char *target, char *replacement)
+{
+    char *pwd;
+    char *holder;
+    int status;
+
+    pwd = ft_getenv("PWD");
+		holder = ft_strreplace(pwd, target, replacement);
+		if(!holder)
+			printf("cd: string not in pwd: %s\n", target);
+		else
+		{
+			status = chdir(holder);
+		  if(status == -1)
+        printf("cd: no such file or directory: %s\n", holder);
+			else
+      printf("%s\n", holder);
+      free(holder);
+    }
+}
+
+static void go_oldpwd()
+{
+    char *oldpwd;
+    int status;
+
+    oldpwd = ft_getenv("OLDPWD");
+		printf("%s\n", oldpwd);
+		status = chdir(oldpwd); 
+		if(status == -1)
+			perror("cd faild");
+}
+
 void cd(char **arg)
 {
 	int size;
 	char path[4096];
-	int status;
-	char *oldpwd;
-	char *pwd;
-	char *holder;
 
 	size = arg_size(arg);
 	getcwd(path, 4096);
 	if(size > 3)
 		printf("cd: too many arguments\n");
 	else if(size == 2 && arg[1][0] == '-' && ft_strlen(arg[1]) == 1)
-	{
-		// print current directory !!!
-		oldpwd = ft_getenv("OLDPWD");
-		printf("%s\n", oldpwd);
-		status = chdir(oldpwd); 
-		if(status == -1)
-			perror("cd faild");
-	}
+    go_oldpwd();		
 	else if((size == 2 && arg[1][0] == '~' && ft_strlen(arg[1]) == 1) || size == 1)
-	{
-		status = chdir(ft_getenv("HOME"));
-		if(status == -1)
-			perror("cd faild !");
-	}
+    go_tohome();
 	else if(size == 3)
-	{
-		pwd = ft_getenv("PWD");
-		holder = ft_strreplace(pwd, arg[1], arg[2]);
-		if(!holder)
-			printf("cd: string not in pwd: %s\n", arg[1]);
-		else
-		{
-			printf("%s\n", holder);
-			chdir(holder);
-		}
-	}
+    go_replcpwd(arg[1], arg[2]);		
 	else if(size == 2 && (arg[1][0] != '~'))
 		go_todir(arg[1]);    
 	else if(size == 2 && arg[1][0] == '~' && ft_strlen(arg[1]) > 1)
-	{
-		holder = ft_getenv("HOME");
-		pwd = ft_strreplace(arg[1], "~", holder);
-		status = chdir(pwd);
-		free(pwd);
-		if(status == -1)
-			printf("cd: no such file or directory: %s\n", arg[1]);
-	}
+    go_tobasedhome(arg[1]);		
 	else 
 	{
 		printf("Unsupported arguments !");
@@ -86,5 +109,4 @@ void cd(char **arg)
 	ft_updateenv("OLDPWD", path);
 	getcwd(path, 4096);
 	ft_updateenv("PWD", path);
-
 }
